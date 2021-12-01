2790c3c7c7ccf250745d79d788dde5f409240f4a
maxyou <hyyou2010@gmail.com>
2021-12-01 23:29:55 +0800

    top app bar

--- app/src/main/java/com/maxporj/purebbs_compose/MainActivity.kt
+++ app/src/main/java/com/maxporj/purebbs_compose/MainActivity.kt
@@ -3,12 +3,14 @@ package com.maxporj.purebbs_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -31,7 +33,42 @@ fun App() {
         val navController = rememberNavController()
         val backstackEntry = navController.currentBackStackEntryAsState()
 
-        Scaffold() {
+        val result = remember { mutableStateOf("") }
+        val expanded = remember { mutableStateOf(false)}
+        val liked = remember { mutableStateOf(true) }
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = "app bar")
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { /* doSomething() */ }) {
+                            Icon(Icons.Filled.Menu, contentDescription = null)
+                        }
+                    },
+                    actions = {
+                        // RowScope here, so these icons will be placed horizontally
+                        IconButton(onClick = { /* doSomething() */ }) {
+                            Icon(Icons.Filled.Create, contentDescription = "Localized description")
+                        }
+                        IconButton(onClick = { /* doSomething() */ }) {
+                            Icon(Icons.Filled.Favorite, contentDescription = "Localized description")
+                        }
+                        IconButton(onClick = { /* doSomething() */ }) {
+                            Icon(Icons.Filled.Delete, contentDescription = "Localized description")
+                        }
+                        IconButton(onClick = { /* doSomething() */ }) {
+                            Icon(Icons.Filled.Settings, contentDescription = "Localized description")
+                        }
+                        IconButton(onClick = { /* doSomething() */ }) {
+                            Icon(Icons.Filled.Share, contentDescription = "Localized description")
+                        }
+                    }
+                )
+            }
+        ) {
             innerPadding ->
             GetNavHost(navController, modifier = Modifier.padding(innerPadding))
         }
