1520a781888e4434f5a41c3c8e0da020cf5fbad3
maxyou <hyyou2010@gmail.com>
2022-01-12 13:23:00 +0800

    add post button

--- app/src/main/java/com/maxporj/purebbs_compose/ui/post/PostPage.kt
+++ app/src/main/java/com/maxporj/purebbs_compose/ui/post/PostPage.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -38,12 +39,15 @@ fun PostLayout(
         modifier = Modifier.padding(15.dp)
     ) {
 
-        Box(
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(40.dp),
-            contentAlignment = Alignment.CenterEnd
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Add")
+            }
             PageRound(
                 current = current,
                 ext = 2,
